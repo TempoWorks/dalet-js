21b264ad944494fd17316d2cf4f88c2eeb92e2d5
Artemy Egorov <artegoser@gmail.com>
2025-03-23 10:38:01 +0300

    move from dalet repo

---
+++ src/daletl/types.ts
@@ -0,0 +1,11 @@
+export interface Page {
+  data: Tag[];
+}
+export type Body = string | Tag[] | null;
+export type Argument = string | number | null;
+
+export interface Tag {
+  id: number;
+  body: Body;
+  argument: Argument;
+}
