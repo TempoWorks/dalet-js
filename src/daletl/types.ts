557217c2b707b39a76447bbd1326ebac2a86169d
Artemy Egorov <artegoser@gmail.com>
2025-03-29 14:46:33 +0300

    fix types

--- src/daletl/types.ts
+++ src/daletl/types.ts
@@ -7,31 +7,33 @@ export type TableRows = { Default: Vec<Tag> } | { Primary: Vec<Tag> };
 export type Body = { Text: string } | { Tags: Vec<Tag> };
 
 export type Tag =
-  | { El: { body: Body } }
-  | { H: { body: Text; heading: HeadingLevel } }
-  | { P: { body: Body } }
-  | { Ul: { body: Vec<Tag> } }
-  | { Ol: { body: Vec<Tag> } }
-  | { Row: { body: Vec<Tag> } }
-  | { Link: { body: Body | null; dref: Text } }
-  | { NavLink: { body: Body | null; dref: Text } }
-  | { Button: { body: Body | null; dref: Text } }
-  | { NavButton: { body: Body | null; dref: Text } }
-  | { Img: { src: Text } }
-  | { Table: { body: Vec<TableRows> } }
-  | { B: { body: Text } }
-  | { I: { body: Text } }
-  | { Bq: { body: Body } }
-  | { FootLink: { footnote: number } }
-  | { FootNote: { body: Text; footnote: number } }
-  | { A: { anchor: Text } }
-  | { S: { body: Text } }
-  | { Sup: { body: Text } }
-  | { Sub: { body: Text } }
-  | { Disc: { body: Body } }
-  | { Carousel: { body: Vec<Tag> } }
-  | { Code: { body: Text; language: Text | null } }
-  | { Pre: { body: Text } }
+  | Partial<{
+      El: { body: Body };
+      H: { body: Text; heading: HeadingLevel };
+      P: { body: Body };
+      Ul: { body: Vec<Tag> };
+      Ol: { body: Vec<Tag> };
+      Row: { body: Vec<Tag> };
+      Link: { body: Body | null; dref: Text };
+      NavLink: { body: Body | null; dref: Text };
+      Button: { body: Body | null; dref: Text };
+      NavButton: { body: Body | null; dref: Text };
+      Img: { src: Text };
+      Table: { body: Vec<TableRows> };
+      B: { body: Text };
+      I: { body: Text };
+      Bq: { body: Body };
+      FootLink: { footnote: number };
+      FootNote: { body: Text; footnote: number };
+      A: { anchor: Text };
+      S: { body: Text };
+      Sup: { body: Text };
+      Sub: { body: Text };
+      Disc: { body: Body };
+      Carousel: { body: Vec<Tag> };
+      Code: { body: Text; language: Text | null };
+      Pre: { body: Text };
+    }>
   | "LineBreak"
   | "HorizontalBreak";
 
